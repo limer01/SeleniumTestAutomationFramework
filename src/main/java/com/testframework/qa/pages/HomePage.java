112ace7a22be7d3d554868e228f527c2c1e34c0b
Liam <lalor95@Gmail.com>
2023-11-13 23:33:04 -0800

    Updated Homepgae and WahtsNewPage elements and link clicks

--- src/main/java/com/testframework/qa/pages/HomePage.java
+++ src/main/java/com/testframework/qa/pages/HomePage.java
@@ -1,7 +1,74 @@
 package com.testframework.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.testframework.qa.base.TestBase;
 
 public class HomePage extends TestBase{
+	
+	@FindBy(xpath="//span[text()=\"What's New\"]")
+	WebElement whatsNewPageLink;
+	
+	@FindBy(xpath="//span[text()='Women']")
+	WebElement womenPageLink;
+	
+	@FindBy(xpath="//span[text()='Men']")
+	WebElement menPageLink;
+	
+	@FindBy(xpath="//span[text()='Gear']")
+	WebElement gearPageLink;
+	
+	@FindBy(xpath="Training")
+	WebElement trainingPageLink;
+	
+	@FindBy(xpath="Sale")
+	WebElement salePageLink;
+	
+	//initialize the Page Object
+	public HomePage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public String verifyHomePageTitle(){
+		return driver.getTitle();
+	}
+	
+	public WhatsNewPage whatsNewPageLink() {
+		whatsNewPageLink.click();
+		return new WhatsNewPage();
+	}
+	
+	public WomenPage womenPageLink() {
+		womenPageLink.click();
+		return new WomenPage();
+	}
+	
+	public MenPage menPageLink() {
+		menPageLink.click();
+		return new MenPage();
+	}
+	
+	public GearPage gearPageLink() {
+		gearPageLink.click();
+		return new GearPage();
+	}
+	
+	public TrainingPage trainingPageLink() {
+		trainingPageLink.click();
+		return new TrainingPage();
+	}
+	
+	public SalePage salePageLink() {
+		salePageLink.click();
+		return new SalePage();
+	}
+	
+	
+	
+	
+		
+	
 
 }
--- src/main/java/com/testframework/qa/pages/WhatsNewPage.java
+++ src/main/java/com/testframework/qa/pages/WhatsNewPage.java
@@ -1,7 +1,73 @@
 package com.testframework.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.testframework.qa.base.TestBase;
 
 public class WhatsNewPage extends TestBase{
+	
+	@FindBy(xpath="//a[@class='logo']/img")
+	WebElement homePageIcon;
+	
+	@FindBy(xpath="//span[text()=\"What's New\"]")
+	WebElement whatsNewPageLink;
+	
+	@FindBy(xpath="//span[text()='Women']")
+	WebElement womenPageLink;
+	
+	@FindBy(xpath="//span[text()='Men']")
+	WebElement menPageLink;
+	
+	@FindBy(xpath="//span[text()='Gear']")
+	WebElement gearPageLink;
+	
+	@FindBy(xpath="Training")
+	WebElement trainingPageLink;
+	
+	@FindBy(xpath="Sale")
+	WebElement salePageLink;
+	
+	
+	public WhatsNewPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public String verifyHomePageTitle(){
+		return driver.getTitle();
+	}
+	
+	public HomePage homePageLink() {
+		homePageIcon.click();
+		return new HomePage();
+	}
+	
+	public WomenPage womenPageLink() {
+		womenPageLink.click();
+		return new WomenPage();
+	}
+	
+	public MenPage menPageLink() {
+		menPageLink.click();
+		return new MenPage();
+	}
+	
+	public GearPage gearPageLink() {
+		gearPageLink.click();
+		return new GearPage();
+	}
+	
+	public TrainingPage trainingPageLink() {
+		trainingPageLink.click();
+		return new TrainingPage();
+	}
+	
+	public SalePage salePageLink() {
+		salePageLink.click();
+		return new SalePage();
+	}
+	
+	
 
 }
